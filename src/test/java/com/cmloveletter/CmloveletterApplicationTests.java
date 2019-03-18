b63b7911bef3335fa45fead741270557fd57ab87
Administrator <145720>
2019-03-18 10:23:21 +0800

    版本控制

---
+++ src/test/java/com/cmloveletter/CmloveletterApplicationTests.java
@@ -0,0 +1,29 @@
+package com.cmloveletter;
+
+import com.cmloveletter.dao.UserMapper;
+import com.cmloveletter.entity.User;
+import com.cmloveletter.service.UserService;
+import com.cmloveletter.service.impl.UserServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CmloveletterApplicationTests {
+
+    @Autowired
+    UserServiceImpl s;
+
+    @Test
+    public void contextLoads() {
+        //int count = mapper.countUser();
+        //System.out.println(list);
+    }
+
+}
+
